53a261da85b0ba2c8ccf9076a261b96babdde931
jake.liu <jake.liu@baozun.com>
2022-06-28 09:47:59 +0800

    cloud native example

---
+++ springboot-sentinel-dashboard/start/src/main/java/com/heidsoft/springbootsentineldashboard/SpringbootSentinelDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.heidsoft.springbootsentineldashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSentinelDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSentinelDashboardApplication.class, args);
+    }
+
+}
---
+++ springboot-sentinel-dashboard/start/src/test/java/com/heidsoft/springbootsentineldashboard/SpringbootSentinelDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heidsoft.springbootsentineldashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSentinelDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
