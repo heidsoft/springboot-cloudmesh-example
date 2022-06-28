915a26c6051866fdf2fd2426eefa73ca198d4fe1
jake.liu <jake.liu@baozun.com>
2022-06-28 13:38:37 +0800

    http response body

--- springboot-redis-example/src/main/java/top/cloudmesh/controller/RedisController.java
+++ springboot-redis-example/src/main/java/top/cloudmesh/controller/RedisController.java
@@ -1,10 +1,19 @@
 package top.cloudmesh.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.cloudmesh.utils.RedisLockUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author heidsoft
@@ -15,6 +24,9 @@ public class RedisController {
 
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    private RedisLockUtil redisLockUtil;
 
     /**
      * 测试redis ops set 操作
@@ -33,6 +45,23 @@ public class RedisController {
         return  opsId;
     }
 
+    @GetMapping("/lock")
+    public void redisLock(){
+        //获取锁
+        Boolean lock = redisLockUtil.tryLock("lock-redis", "12000", 10);
+        if (null != lock && lock) {
+            try {
+                //TODO 业务处理
+                System.out.println("获取lock 处理");
+            } finally {
+                //释放锁
+                Boolean release = redisLockUtil.releaseLock("lock-redis", "12000");
+            }
+        } else {
+            System.out.println("获取锁失败");
+        }
+    }
+
 
     /**
      * 测试类
@@ -41,5 +70,19 @@ public class RedisController {
     @GetMapping("/test")
     public String test(){
         return "test redis";
+    }
+
+    /**
+     * 带响应实体的方式
+     * @return
+     */
+    @GetMapping(path = "/hello", produces= MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> sayHello() throws JSONException {
+        //Get data from service layer into entityList.
+        List<JSONObject> entities = new ArrayList<JSONObject>();
+        JSONObject entity = new JSONObject();
+        entity.put("test",1);
+        entities.add(entity);
+        return new ResponseEntity<Object>(entities, HttpStatus.OK);
     }
 }
